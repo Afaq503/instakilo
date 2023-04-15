36176b2982597e4a7a8a7eb3a9d7afc9b238097f
Afaq503 <afaqg503@gmqil.com>
2023-04-16 00:22:09 +0500

    i

--- src/views/CameraView.vue
+++ src/views/CameraView.vue
@@ -1,72 +1,35 @@
 <template>
-  <div class="camera-box">
-    <div style="display: flex; justify-content: center">
-      <img
-        style="height: 25px"
-        v-if="isCameraOpen"
-        src="https://img.icons8.com/material-outlined/50/000000/camera--v2.png"
-        class="button-img camera-shoot"
-        @click="capture" />
-      <div class="camera-button">
-        <button
-          type="button"
-          class="button is-rounded cam-button"
-          style="margin-left: 40%; background-color: white; border: 0px"
-          @click="toggleCamera">
-          <span v-if="!isCameraOpen"
-            ><img
-              style="height: 25px"
-              class="button-img"
-              src="https://img.icons8.com/material-outlined/50/000000/camera--v2.png"
-          /></span>
-          <span v-else
-            ><img
-              style="height: 25px"
-              class="button-img"
-              src="https://img.icons8.com/material-outlined/50/000000/cancel.png"
-          /></span>
-        </button>
-      </div>
-    </div>
-    <div style="height: 200px">
-      <div v-if="isCameraOpen" class="camera-canvas">
-        <video
-          ref="camera"
-          :width="canvasWidth"
-          :height="canvasHeight"
-          autoplay></video>
-        <canvas
-          v-show="false"
-          id="photoTaken"
-          ref="canvas"
-          :width="canvasWidth"
-          :height="canvasHeight"></canvas>
-      </div>
-    </div>
+  <div>
+    <video ref="video">Streming is unavialble</video>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Camera",
-  components: {},
+  name: "camera",
+  mounted() {
+    this.video = this.$refs.video;
+    this.startCapture();
+  },
+  methods: {
+    startCapture() {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+          this.video.srcObject = stream;
+          this.video.play();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
   data() {
     return {
-      isCameraOpen: false,
-      canvasHeight: 200,
-      canvasWidth: 190,
-      items: [],
+      video: null,
     };
   },
 };
 </script>
 
-<style scoped>
-.camera-box {
-  border: 1px dashed #d6d6d6;
-  border-radius: 4px;
-  padding: 2px;
-  width: 80%;
-  min-height: 300px;
-}
-</style>
+<style></style>
